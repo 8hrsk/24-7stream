3b507582c670384af51e42d1555779e6bbe97213
= <=>
2023-12-24 23:53:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import youtube from "youtube-live-streaming";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const __dirname = new URL('.', import.meta.url).pathname
+const assets = path.join(__dirname, 'app/assets/');
+const { API, AUDIO } = process.env;
+let { VIDEO } = process.env;
+VIDEO = assets + VIDEO;
+console.log(API, VIDEO, AUDIO);
+
+youtube(API, VIDEO, AUDIO);
